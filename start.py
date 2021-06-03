25e53b258f51e913f520ca64693b945de65de96e
GAMERFUCKER <bsdkbhaag248@gmail.com>
2021-06-03 21:30:51 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,6 +1,6 @@
 from os import system 
-system ("git clone https://GitHub.com/gamerfuckerofficial/Gamerzbot && cd Gamerzbot && python3 -m Gamerzbot")
+system ("git clone https://GitHub.com/gamerfuckerofficial/Gamerzbot && cd Gamerzbot && python3 -m telebot")
 # try af
 print("𝙾𝙷 𝚂𝙺𝙴𝙼 𝚂𝚈𝚂𝚃𝙴𝙼 𝙶𝙾𝚃 𝙲𝚁𝙰𝚂𝙷𝙴𝙳")
-system("python3 -m Gamerzbot")
+system("python3 -m telebot")
 print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @CheemsBotChat")
